bd09fde9507fbb89b3ab0ae896430ad7d5ea833d
Angathan003 <angathan003@gmail.com>
2023-07-06 19:32:54 +0530

    Change the code using array

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 document.getElementById("calculateBtn").addEventListener("click", fdRate);
 
 function fdRate() {
-    const months = Number(document.getElementById("input").value);
-    let rate = 0;
+  const months = Number(document.getElementById("input").value);
+  const rates = [5.8, 6.5, 6.8, 7];
+  let rate = 0;
 
-    if (months < 3) {
-        rate = 5.8;
-    } else if (months >= 3 && months <= 6) {
-        rate = 6.5;
-    } else if (months >= 7 && months <= 9) {
-        rate = 6.8;
-    } else {
-        rate = 7;
-    }
-    
-    let result = document.getElementById("result");
-    result.textContent = "Your interest rate is " + rate + "%";
+  if (months < 3) {
+    rate = rates[0];
+  } else if (months >= 3 && months <= 6) {
+    rate = rates[1];
+  } else if (months >= 7 && months <= 9) {
+    rate = rates[2];
+  } else {
+    rate = rates[3];
+  }
 
-
+  let result = document.getElementById("result");
+  result.textContent = "Your interest rate is " + rate + "%";
 }
